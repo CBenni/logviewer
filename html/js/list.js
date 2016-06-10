8d67e1811c9b421b5b0742ade46d1daff2927445
CBenni <levelcarnage@gmail.com>
2016-06-10 02:56:10 +0200

    Sorted channels on the frontpage

--- html/js/list.js
+++ html/js/list.js
@@ -2,23 +2,27 @@ var logviewerApp = angular.module("logviewerApp");
 
 logviewerApp.controller("ChannelListController", function($rootScope, $scope, $http, $stateParams){
 	$scope.channels = [];
-	$scope.streams = {};
 	
 	var updateChannels = function(){
 		$http.jsonp("/api/channels/?callback=JSON_CALLBACK").then(function(response) {
 			var newchannels = [];
+			var channelnames = "";
+			var channelDict = {};
 			for(var i=0;i<response.data.length;++i) {
-				newchannels.push(response.data[i].name);
+				var name = response.data[i].name;
+				var channelObj = {name: name, live: false};
+				channelDict[name] = channelObj;
+				newchannels.push(channelObj);
+				if(channelnames) channelnames += ",";
+				channelnames += name;
 			}
-			$scope.channels = newchannels;
-			$http.jsonp("https://api.twitch.tv/kraken/streams?channel="+newchannels.join(",")+"&callback=JSON_CALLBACK").then(function(response2){
+			$http.jsonp("https://api.twitch.tv/kraken/streams?channel="+channelnames+"&callback=JSON_CALLBACK").then(function(response2){
 				var streams = response2.data.streams;
-				var newstreams = {};
 				for(var j=0;j<streams.length;++j) {
-					newstreams[streams[j].channel.name] = streams[j];
+					channelDict[streams[j].channel.name].live = true;
 				}
-				$scope.streams = newstreams;
 			});
+			$scope.channels = newchannels;
 		});
 	}
 	
